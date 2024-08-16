b672fa0dd43528c191acbcbf3e49111e853eb463
Oksana <benefit989@gmail.com>
2024-08-16 15:45:47 +0200

    first commit

---
+++ lesson2/clw1/src/index.js
@@ -0,0 +1,21 @@
+import { legacy_createStore as createStore } from 'redux';
+
+const increment= {
+  type: 'COUNTER/INCREMENT',
+};
+const decrement = {
+  type: 'COUNTER/DECREMENT',
+};
+
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case 'COUNTER/INCREMENT':
+      return state + 1;
+    case 'COUNTER/DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
