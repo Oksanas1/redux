64f5a8414844e153ae3bac1edd5263e03958c321
Oksana <benefit989@gmail.com>
2024-09-15 23:05:52 +0200

    first commit

---
+++ lesson5/clw1/src/users/UsersList.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { connect } from 'react-redux';
+import { prevPage, nextPage } from './users.actions';
+import Pagination from "./Pagination";
+import User from "./User";
+import { currentPageSelectors, usersListSelectors } from "./users.selectors";
+
+const UsersList = ({ users, currentPage, goPrev, goNext, itemsPerPage }) => {
+  const startIndex = currentPage * itemsPerPage;
+  const paginatedUsers = users.slice(startIndex, startIndex + itemsPerPage);
+
+  return (
+    <div>
+      <Pagination
+        goPrev={goPrev}
+        goNext={goNext}
+        currentPage={currentPage}
+        totalItems={users.length}
+        itemsPerPage={itemsPerPage}
+      />
+      <ul className="users">
+        {paginatedUsers.map(user => (
+          <User key={user.id} {...user} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return ({
+    users: usersListSelectors(state),
+    currentPage: currentPageSelectors(state),
+    itemsPerPage: 3,
+  });
+};
+
+const mapDispatchToProps = {
+  goPrev: prevPage,
+  goNext: nextPage,
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export default connector(UsersList);
---
+++ lesson5/clw1/src/users/users.actions.js
@@ -0,0 +1,5 @@
+export const PREV_PAGE = 'PAGE/PREV';
+export const NEXT_PAGE = 'PAGE/NEXT';
+
+export const prevPage = () => ({type: PREV_PAGE});
+export const nextPage = () => ({type: NEXT_PAGE});
