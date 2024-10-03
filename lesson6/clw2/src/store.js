b88768e0067cacbbd04c4c4f3dc0595b39ec3ee1
Oksana <benefit989@gmail.com>
2024-10-04 00:35:49 +0200

    fix

--- lesson6/clw2/src/store.js
+++ lesson6/clw2/src/store.js
@@ -18,7 +18,7 @@ const reducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(
-  applyMiddleware(thunk, middleware)
+  applyMiddleware(middleware, thunk)
 ));
 
 export default store;
