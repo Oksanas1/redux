0922f062fe493412bb19bd9bf9ed474384189280
Oksana <benefit989@gmail.com>
2024-10-07 12:33:07 +0200

    fix

--- lesson6/clw3/src/weather/weather.actions.js
+++ lesson6/clw3/src/weather/weather.actions.js
@@ -1,13 +1,18 @@
-import { getWeatherData } from "./weather.gateway";
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities';
 
 export const GET_WEATHER_DATA = 'GET_WEATHER_DATA';
 
-export const weatherDataRecieved = weatherData => ({type: GET_WEATHER_DATA, payload: {weatherData}});
+export const getWeatherData = weatherData => ({type: GET_WEATHER_DATA, payload: {weatherData}});
 export const fetchWeatherData = () => {
   return function(dispatch) {
-    getWeatherData()
+    fetch(`${baseUrl}`).then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    })
       .then(weatherData => {
-        return dispatch(weatherDataRecieved(weatherData));
+        return dispatch(getWeatherData(weatherData));
       })
       .catch(err => {
         console.error(err);
