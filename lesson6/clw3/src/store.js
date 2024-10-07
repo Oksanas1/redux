91a9cd599704305b2e77d317f825ee3ff9682930
Oksana <benefit989@gmail.com>
2024-10-07 12:19:21 +0200

    fix

--- lesson6/clw3/src/store.js
+++ lesson6/clw3/src/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
 import { thunk } from 'redux-thunk';
 import weatherReducer from './weather/weather.reducer';
 
-const store = createStore(weatherReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(weatherReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
